64333fd61b3cc6057925fcbf32edc17657f2fb10
Nightwing52 <thebomb99534@gmail.com>
2023-05-24 14:05:32 -0500

    Revert "Added navbar boilerplate."
    This reverts commit b855337003750dfb1064b8cc643a950228e03b7d.

--- js/nav.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-  // Toggle navigation menu on burger icon click
-  navLinks.classList.toggle('nav-active');
-  // Toggle burger icon animation
-  burger.classList.toggle('toggle');
-});
